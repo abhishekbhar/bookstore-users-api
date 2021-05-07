d953c2497c6e06d5f14ddaf0498fc895254ad78d
Abhishek Bhardwaj <volage.abhishek@gmail.com>
2021-05-07 14:15:05 +0000

    application bootstrap

---
+++ app/application.go
@@ -0,0 +1,17 @@
+package app
+
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+
+var (
+	router = gin.Default()
+)
+
+
+func StartApp(){
+	mapUrls()
+	router.Run(":8080")
+}
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+
+import (
+	"github.com/abhishekbhar/bookstore-users-api/app"
+)
+
+
+func main(){
+	app.StartApp()
+}
