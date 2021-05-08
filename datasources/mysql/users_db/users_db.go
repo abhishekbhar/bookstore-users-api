5bdc3a977a7778c81ad65fa5186243db7ee62351
Abhishek Bhardwaj <volage.abhishek@gmail.com>
2021-05-08 11:28:30 +0000

    added mysql config to connect mysql-container

--- datasources/mysql/users_db/users_db.go
+++ datasources/mysql/users_db/users_db.go
@@ -7,15 +7,37 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 )
 
+/*
+*
+* 		To connect to mysql server running on another container use following commands:
+* 		--> Create network 
+*				docker network create {network name}
+* 		--> Add container to the network
+*				docker network connect {network name} {dev container name}
+*				docker network connect {network name} {mysql container name}
+* 		--> List all the container on the network with ip address
+* 				docker network inspect {network name} 
+* 
+*/
+
+
+
 var (
 	Client *sql.DB
+)
+
+const (
+	mysql_users_username  = ""
+	mysql_users_password  = ""
+	mysql_users_host      = "172.18.0.3" //comes from docker network config
+	mysql_users_schema    = ""
 )
 
 func init() {
 	dataSourceName := fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8",
 		"root",
 		"toor",
-		"127.0.0.1",
+		mysql_users_host,
 		"users_db",
 	)
 
