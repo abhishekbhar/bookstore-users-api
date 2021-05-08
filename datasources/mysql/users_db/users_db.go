1bafd056906a1828d5a40923a98c0116a10addc1
Abhishek Bhardwaj <volage.abhishek@gmail.com>
2021-05-08 11:05:35 +0000

    added mysql support

---
+++ datasources/mysql/users_db/users_db.go
@@ -0,0 +1,33 @@
+package users_db
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	_ "github.com/go-sql-driver/mysql"
+)
+
+var (
+	Client *sql.DB
+)
+
+func init() {
+	dataSourceName := fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8",
+		"root",
+		"toor",
+		"127.0.0.1",
+		"users_db",
+	)
+
+	var err error
+	Client, err = sql.Open("mysql", dataSourceName)
+
+	if err != nil {
+		panic(err)
+	}
+
+	if err := Client.Ping(); err != nil {
+		panic(err)
+	}
+	log.Printf("database sucessfully configured!!")
+}
