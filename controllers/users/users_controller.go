f3888a95074fe42c991f0272ee1a3973e9bebfe3
Abhishek Bhardwaj <volage.abhishek@gmail.com>
2021-05-08 03:50:38 +0000

    implemented api using in memory map, added consitent error model

--- controllers/users/users_controller.go
+++ controllers/users/users_controller.go
@@ -3,45 +3,54 @@ package users
 
 import (
 	"net/http"
-	"io/ioutil"
-	"fmt"
-	"encoding/json"
+	"strconv"
 	"github.com/gin-gonic/gin"
 	"github.com/abhishekbhar/bookstore-users-api/domain/users"
 	"github.com/abhishekbhar/bookstore-users-api/services"
+	"github.com/abhishekbhar/bookstore-users-api/utils/errors"
 )
 
 
 func CreateUser(c *gin.Context) {
 	var user users.User
-	bytes, err := ioutil.ReadAll(c.Request.Body)
-	if err != nil {
-		//TODO: Handle error
-		return
-	}
-	if err := json.Unmarshal(bytes, &user); err != nil {
-		fmt.Println(err.Error())
+
+	if err := c.ShouldBindJSON(&user); err != nil {
+		restErr := errors.NewBadRequestError("invalid json body")
+		c.JSON(restErr.Status, restErr)
 		return
 	}
 
 	result, saveError := services.CreateUser(user)
 
 	if saveError != nil {
-		//TODO: Handle User creation Error
+		c.JSON(saveError.Status, saveError)
 		return
 	}
 
 	c.JSON(http.StatusCreated, result)
-	// c.String(http.StatusNotImplemented, "implement me!")
 
 }
 	
 
 func GetUser(c *gin.Context) {
-	c.String(http.StatusNotImplemented, "implement me!")
+	userId, userErr := strconv.ParseInt(c.Param("user_id"), 10,64)
+	if userErr != nil {
+		err := errors.NewBadRequestError("user id should be a number")
+		c.JSON(err.Status, err)
+		return
+	}
+
+	user, getError := services.GetUser(userId)
+	if getError != nil {
+		c.JSON(getError.Status, getError)
+		return
+	}
+
+	c.JSON(http.StatusOK, user)
 }
 
 
 func SearchUser(c *gin.Context) {
+
 	c.String(http.StatusNotImplemented, "implement me!")
 }
--- services/users_service.go
+++ services/users_service.go
@@ -3,10 +3,36 @@ package services
 
 import (
 	"github.com/abhishekbhar/bookstore-users-api/domain/users"
+	"github.com/abhishekbhar/bookstore-users-api/utils/errors"
 )
 
 
 
-func CreateUser(user users.User) (*users.User, error){
+func CreateUser(user users.User) (*users.User, *errors.RestErr){
+	if err := user.Validate(); err != nil {
+		return nil, err
+	}
+
+	if err := user.Save(); err != nil {
+		return nil, err
+	}
+
 	return &user,nil
+}
+
+
+func GetUser(userId int64) (*users.User, *errors.RestErr){
+	if userId <= 0 {
+		return nil, errors.NewBadRequestError("invalid user id")
+	}
+
+	user := &users.User{
+		Id: userId,
+	}
+
+	if err := user.Get(); err != nil {
+		return nil, err
+	}
+	
+	return user,nil
 }
