1eac5def84603e599525c868e055687d04ccf3b0
Abhishek Bhardwaj <volage.abhishek@gmail.com>
2021-05-09 11:39:20 +0000

    added mysql query to create and delete users

--- datasources/mysql/users_db/users_db.go
+++ datasources/mysql/users_db/users_db.go
@@ -1,50 +1,23 @@
 package users_db
 
 import (
-	//"os"
 	"database/sql"
 	"fmt"
 	"log"
 	_ "github.com/go-sql-driver/mysql"
 )
 
-/*
-*
-* 		To connect to mysql server running on another container use following commands:
-* 		--> Create network 
-*				docker network create {network name}
-* 		--> Add container to the network
-*				docker network connect {network name} {dev container name}
-*				docker network connect {network name} {mysql container name}
-* 		--> List all the container on the network with ip address
-* 				docker network inspect {network name} 
-* 
-*/
-
-
-const (
-	mysql_users_username  = "mysql_users_username "
-	mysql_users_password  = "mysql_users_password"
-	mysql_users_host      = "mysql_users_host" 		//comes from docker network config
-	mysql_users_schema    = "mysql_users_schema"
-)
-
-
 var (
 	Client *sql.DB
-	// username = os.Getenv(mysql_users_username)
-	// password = os.Getenv(mysql_users_password)
-	// host 	 = os.Getenv(mysql_users_host)
-	// schema 	 = os.Getenv(mysql_users_schema)
-	username = "root"
-	password = "toor"
-	host 	 = "127.18.0.3"
-	schema 	 = "users_db"
-	
 )
 
 func init() {
-	dataSourceName := fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8",username,password,host,schema)
+	dataSourceName := fmt.Sprintf("%s:%s@unix(%s)/%s?charset=utf8",
+		"newuser",
+		"password",
+		"/var/run/mysqld/mysqld.sock",
+		"users_db",
+	)
 
 	var err error
 	Client, err = sql.Open("mysql", dataSourceName)
--- domain/users/user_dao.go
+++ domain/users/user_dao.go
@@ -2,9 +2,17 @@ package users
 
 import (
 	"fmt"
+	"strings"
 	"github.com/abhishekbhar/bookstore-users-api/utils/errors"
 	"github.com/abhishekbhar/bookstore-users-api/utils/date_utils"
 	"github.com/abhishekbhar/bookstore-users-api/datasources/mysql/users_db"
+)
+
+
+const (
+	errNoRow	    = "no rows in result set"
+	queryInsertUser = "INSERT INTO users(first_name, last_name, email, date_created) VALUES(?,?,?,?);"
+	queryGetUser    = "SELECT id, first_name, last_name, email, date_created FROM users WHERE id=?;"
 )
 
 var (
@@ -17,33 +25,74 @@ func(user *User) Get()  *errors.RestErr{
 		panic(err)
 	}
 
-
-	result := usersDB[user.Id]
-
-	if result == nil {
-		return errors.NewNotFoundError(fmt.Sprintf("user %d not found", user.Id))
+	stmt, err := users_db.Client.Prepare(queryGetUser)
+	if err != nil {
+		return errors.NewInternalServerError(err.Error())
 	}
-	user.Id 		= result.Id
-	user.FirstName 	= result.FirstName
-	user.LastName   = result.LastName
-	user.Email		= result.Email
-	user.DateCreated= result.DateCreated
+	defer stmt.Close()
+
+
+	result := stmt.QueryRow(user.Id)
+	if err := result.Scan(&user.Id, &user.FirstName, &user.LastName, &user.Email, &user.DateCreated); err != nil {
+		if strings.Contains(err.Error(), errNoRow) {
+			return errors.NewNotFoundError(fmt.Sprintf("user %d not found", user.Id))
+		}
+		return errors.NewInternalServerError(
+			fmt.Sprintf("error when trying to get user %d: %s", user.Id, err.Error()),
+		)
+	}
+
+	// result := usersDB[user.Id]
+
+	// if result == nil {
+	// 	return errors.NewNotFoundError(fmt.Sprintf("user %d not found", user.Id))
+	// }
+	// user.Id 		= result.Id
+	// user.FirstName 	= result.FirstName
+	// user.LastName   = result.LastName
+	// user.Email		= result.Email
+	// user.DateCreated= result.DateCreated
 
 	return nil
 }
 
 func (user *User) Save() *errors.RestErr {
 
-	current := usersDB[user.Id]
-	if current != nil {
-		if current.Email == user.Email {
-			return errors.NewBadRequestError(fmt.Sprintf("email %s already registered", user.Email))
-		}
-		return errors.NewBadRequestError(fmt.Sprintf("user %d already exists", user.Id))
+	
+	stmt, err := users_db.Client.Prepare(queryInsertUser)
+
+	if err != nil {
+		return errors.NewInternalServerError(err.Error())
 	}
 
-	user.DateCreated = date_utils.GetNowString()
+	defer stmt.Close()
 
-	usersDB[user.Id] = user
+	user.DateCreated = date_utils.GetNowString()
+	insertResult, err := stmt.Exec(user.FirstName, user.LastName, user.Email, user.DateCreated)
+
+	if err != nil {
+		return errors.NewInternalServerError(fmt.Sprintf("Error trying to save user: %s", err.Error()))
+	}
+
+
+	userId, err := insertResult.LastInsertId()
+
+	if err != nil {
+		return errors.NewInternalServerError(fmt.Sprintf("Error trying to save user: %s", err.Error()))
+	}
+
+
+
+	// current := usersDB[user.Id]
+	// if current != nil {
+	// 	if current.Email == user.Email {
+	// 		return errors.NewBadRequestError(fmt.Sprintf("email %s already registered", user.Email))
+	// 	}
+	// 	return errors.NewBadRequestError(fmt.Sprintf("user %d already exists", user.Id))
+	// }
+
+	// user.DateCreated = date_utils.GetNowString()
+
+	user.Id = userId
 	return nil
 }
